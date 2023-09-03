a0760133058fd7dec5be3c572e06f75db6bf8ff4
AsiimovNT <nihalasiimov420@gmail.com>
2023-09-04 00:27:38 +0530

    boob

--- LAB2/DAA/topologicalsort.cpp
+++ LAB2/DAA/topologicalsort.cpp
@@ -7,7 +7,8 @@ private:
 	         vector<int> adj[]) {
 		vis[node] = 1;
 		for (auto it : adj[node]) {
-			if (!vis[it]) dfs(it, vis, st, adj);
+			if (!vis[it]) 
+            dfs(it, vis, st, adj);
 		}
 		st.push(node);
 	}
